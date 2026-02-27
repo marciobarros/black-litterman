0f47fc25a51700e97fd7a0607b56f5ad1b9f623f
Marcio Barros <marcio.barros@gmail.com>
2026-02-27 12:04:01 -0300

    Ajuste nos calculos de matrizes vazias

--- src/servicos/servico-black-litterman.js
+++ src/servicos/servico-black-litterman.js
@@ -317,17 +317,17 @@ var modeloBlackLitterman = function() {
         var w_sobre_tau = calculaMatrizWSobreTau(omega, parametros.tau)
 
         // Calcula o vetor lambda
-        var mx = (matriz_opiniao_transposta.length == 0) ? [] : multiplicaMatrizes(covariancias, matriz_opiniao_transposta)
+        var mx = (matriz_opiniao_transposta.length == 0) ? covariancias : multiplicaMatrizes(covariancias, matriz_opiniao_transposta)
         var my = multiplicaMatrizEscalar(multiplicaMatrizes(participacao_opiniao, mx), 1.0 / (1.0 + parametros.tau))
         var mz = multiplicaMatrizEscalar(omega, 1.0 / parametros.tau)
         var A = (my.length == 0) ? mz : somaMatrizes(mz, my)
 
         var A_invertida = inverteMatriz(A)
-        var c11 = (participacao_opiniao.length == 0) ? [] : multiplicaMatrizes(A_invertida, participacao_opiniao)
+        var c11 = (participacao_opiniao.length == 0) ? A_invertida : multiplicaMatrizes(A_invertida, participacao_opiniao)
         var componente_lambda = multiplicaMatrizEscalar(multiplicaMatrizes(c11, covariancias), 1.0 / (1.0 + parametros.tau))
 
         var c12 = multiplicaMatrizEscalar(omega_invertido, parametros.tau)
-        var c13 = (retornos_opiniao.length == 0) ? [] : multiplicaMatrizes(c12, retornos_opiniao)
+        var c13 = (retornos_opiniao.length == 0) ? c12 : multiplicaMatrizes(c12, retornos_opiniao)
         var lambda_termo1 = multiplicaMatrizEscalar(c13, 1.0 / parametros.aversaoRisco)
 
         var lambda_termo2 = multiplicaMatrizEscalar(multiplicaMatrizes(componente_lambda, capitalizacoes), -1.0 / 100.0)
